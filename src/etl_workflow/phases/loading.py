24294e19019ee9025977da6ad7944307caaef097
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-09 23:11:57 +0200

    Implement loading phase for ETL workflow
    This code sets up the loading phase for the ETL workflow, including environment variable loading, Hopsworks login, and feature store creation.

--- src/etl_workflow/phases/loading.py
+++ src/etl_workflow/phases/loading.py
@@ -1,1 +1,37 @@
 
+import pandas as pd
+import hopsworks
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+## get the API_KEY and PROJECT_NAME from dotenv file:
+api_key=os.getenv("API_KEY")
+project_name=os.getenv("PROJECT_NAME")
+
+
+## login hopsworks:
+project=hopsworks.login(
+    api_key_value=api_key,
+    project=project_name
+)
+
+## create the feature store
+feature_store=project.get_feature_store()
+
+## create the feature view:
+feature_name="medical_costs"
+feature_version="1"
+
+feature_description="features for medical cost ML system"
+
+feature_view=feature_store.get_or_create_group(
+    name=feature_name,
+    version=feature_version,
+    description=feature_description,
+    primary_key=['userId'],
+    event=['time_stamp']
+)
+
