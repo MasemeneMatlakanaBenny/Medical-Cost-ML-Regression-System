796c1983a9c474007f5bdec47af8376ff8daeb8f
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-09 23:09:04 +0200

    added the data transformation methods

--- src/etl_workflow/phases/transform.py
+++ src/etl_workflow/phases/transform.py
@@ -1,1 +1,19 @@
+## here we transform the dataset and select the most relevant one that will be crucial ahead of the entire ML Workflow
+import pandas as pd
+import numpy as np
+from datetime import datetime
 
+##load the extracted df:
+df=pd.read_csv("data/extracted_df.csv")
+
+df["userId"]=np.arange(1,len(df)+1)
+df["time_event"]=datetime.now()
+
+
+## if no insurance -> fill with nulls
+df["insurance_type"]=df["insurance_type"].fillna("No Insurance")
+
+print(df.info()) ## to check if there are any nulls remaining or not
+
+## save the df now:
+df.to_csv("transformed_df.csv",index=False)
