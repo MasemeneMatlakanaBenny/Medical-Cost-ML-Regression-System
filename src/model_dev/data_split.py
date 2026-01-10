776a41f205a8ad2e89c769b3d53405e230393a3a
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-10 21:43:35 +0200

    Implement data splitting and saving for model training
    Added functionality to split numerical data into training and testing sets and save them as CSV files.

--- src/model_dev/data_split.py
+++ src/model_dev/data_split.py
@@ -1,1 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
+df=pd.read_csv("data/loaded_df.csv")
+
+
+num_data=df.select_dtypes(include='number')
+
+train_df,test_df=train_test_split(num_data,test_size=0.2,random_state=42)
+
+
+## save both the train and test sets to the data file:
+train_df.to_csv("data/train_data.csv",index=False)
+test_df.to_csv("data/test_data.csv",index=False)
