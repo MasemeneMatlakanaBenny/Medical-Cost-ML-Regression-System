e9f2429350e25406da7f080b3249dd57bb5af720
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-10 21:45:29 +0200

    Implement training and saving of linear regression model
    Added code to train and save a linear regression model using joblib.

--- src/model_dev/linear_reg_model.py
+++ src/model_dev/linear_reg_model.py
@@ -1,1 +1,18 @@
+import joblib
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from lib.configs import X_train_y_train
 
+train_df=pd.read_csv("data/train_data.csv")
+
+
+## load the X_train and y_train
+X_train,y_train=X_train_y_train(train_df=train_df)
+
+## fit the model:
+model=LinearRegression()
+
+model.fit(X_train,y_train)
+
+## save the model with joblib
+joblib.dump(model,"src/models/linear_reg_model.pkl")
