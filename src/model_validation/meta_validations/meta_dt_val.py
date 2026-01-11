97498dbe31f95e9dea14afc90392cb96872c655d
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-11 21:27:06 +0200

    Update meta_dt_val.py
    added the data for model quality checks

--- src/model_validation/meta_validations/meta_dt_val.py
+++ src/model_validation/meta_validations/meta_dt_val.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import great_expectations as gx
+import joblib
+from typing import List
+from great_expectations.expectations.expectation import ExpectationConfiguration
+from lib.model_validations import create_model_batch,create_metric_expectations,validate_metric_expectations
 
+## get the results_df:
+results_df=pd.read_csv("model_quality_checks/dt_validations.csv")
+
+
+## create the batch:
+batch=create_model_batch(df=results_df)
+
+## create the expectations:
+expectation=gx.expectations.ExpectColumnDistinctValuesToEqualSet(
+    value_set=["success"],column="results"
+)
+
+## expectations labels:
+meta_validation=batch.validate(expectation)[0]
+
+print(meta_validation)
