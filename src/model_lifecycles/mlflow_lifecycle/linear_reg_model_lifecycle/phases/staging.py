47e1df9c4d39268cfb09410552c986fa106bfaed
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-11 03:39:30 +0200

    Implement model version staging in MLflow
    Added functionality to transition model version to staging phase in MLflow.

--- src/model_lifecycles/mlflow_lifecycle/linear_reg_model_lifecycle/phases/staging.py
+++ src/model_lifecycles/mlflow_lifecycle/linear_reg_model_lifecycle/phases/staging.py
@@ -1,1 +1,21 @@
+import mlflow
+import mlflow.sklearn
+from lib.mlflow_configs import set_mlflow_host,set_mlflow_running_experiment,mlflow_client
 
+
+## set the mlflow host and experiment within the mlflow server:
+set_mlflow_host()
+set_mlflow_running_experiment()
+
+## create the client -> will be used to move the model from the registry to staging phase
+client=mlflow_client()
+
+model_name="linear_model"
+model_version=1
+
+## stage the model:
+client.transition_model_version_stage(
+    name=model_name,
+    version=model_version,
+    stage="staging"
+)
