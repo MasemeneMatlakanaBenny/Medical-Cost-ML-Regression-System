30617ad5f34be1a1325a9738889b26d672d7c124
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-11 23:32:16 +0200

    Update inference_pipeline.py

--- src/pipelines/inference_pipeline.py
+++ src/pipelines/inference_pipeline.py
@@ -1,1 +1,39 @@
+import pandas as pd
+import joblib
+from prefect import task,flow
+
+@task
+def get_infer_data(path):
+    """
+    """
+    df=pd.read_csv(path)
+
+    return df
+
+@task
+def linear_model_pred(infer_data:pd.DataFrame):
+    """
+    Docstring for linear_model_pred
+    
+    :param infer_data: new data to predict on
+    :type infer_data: pd.DataFrame
+    """
+
+    model=joblib.load("src/models/linear_reg_model.pkl")
+    X_df=infer_data.drop("annual_medical_cost")
+
+
+    predictions=model.predict(X_df)
+
+    return predictions
+
+@flow
+def inference_workflow(df_path:str):
+    """
+    """
+    infer_df=get_infer_data(path=df_path)
+
+    predictions=linear_model_pred(infer_data=infer_df)
+
+    return predictions
 
