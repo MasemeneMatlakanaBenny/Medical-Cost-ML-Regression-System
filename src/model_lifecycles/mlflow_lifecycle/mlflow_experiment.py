ef63887d7c19280874d5a59f96023ad9e374c8bb
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-12 00:39:23 +0200

    Update mlflow_experiment.py

--- src/model_lifecycles/mlflow_lifecycle/mlflow_experiment.py
+++ src/model_lifecycles/mlflow_lifecycle/mlflow_experiment.py
@@ -1,6 +1,9 @@
 import mlflow
-from lib.mlflow_configs import create_experiment
+from lib.mlflow_configs import create_experiment,set_mlflow_host,load_project_tags
+
+set_mlflow_host()
 
 create_experiment(
-  name="medical_cost_exp",
+  experiment_name="medical_cost_exp",
+  tags=load_project_tags()
 )
