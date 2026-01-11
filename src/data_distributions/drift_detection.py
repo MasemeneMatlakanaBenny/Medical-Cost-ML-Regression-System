ccd61e728458d6d5e613916d935470b26ea80557
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-11 04:01:19 +0200

    Update drift_detection.py
    drift detection

--- src/data_distributions/drift_detection.py
+++ src/data_distributions/drift_detection.py
@@ -1,1 +1,40 @@
+import numpy as np
+import pandas as pd
+
+np.Inf=np.inf
+np.NINF=np.inf
+
+from deepchecks.tabular.dataset import Dataset
+from deepchecks.tabular.checks import FeatureDrift
+from lib.configs import X_train_y_train
+
+X_train,y_train=X_train_y_train()
+
+train_df=pd.read_csv("data/train_df.csv")
+
+test_df=pd.read_csv("data/test_df.csv")
+
+train_checks=Dataset(train_df,cat_features=[])
+test_checks=Dataset(test_df,cat_features=[])
+
+
+drift=FeatureDrift()
+
+results=drift.run(train_dataset=train_checks,test_dataset=test_checks)
+
+features=X_train.columns
+drift_scores=[]
+
+for feature in features:
+    drift_score=results[feature]['Drift score']
+
+    drift_scores.append(drift_score)
+
+drift_df=pd.DataFrame(
+    {"features":feature,
+     "drift_score":drift_scores}
+)
+
+
+drift_df.to_csv("data/drift_scores.csv")
 
