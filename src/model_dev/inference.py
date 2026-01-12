b3f3c84e87f392cab2e111c63d2b66609c5db860
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-12 05:40:36 +0200

    Update inference.py

--- src/model_dev/inference.py
+++ src/model_dev/inference.py
@@ -1,1 +1,22 @@
+## make predictions on test data:
+import joblib
+import pandas as pd
+from lib.configs import X_train_y_train,compute_model_metrics
 
+
+test_df=pd.read_csv("data/test_data.csv")
+
+## get the X_test and y_test:
+X_test,y_test=X_train_y_train(test_df)
+
+## get the model:
+model=joblib.load("src/models/linear_reg_model.pkl")
+
+
+## get the predictions:
+predictions=model.predict(X_test)
+
+## save as df:
+pred_df=pd.DataFrame({"predictions":predictions})
+## save the predictions:
+pred_df.to_csv("data/predictions.csv",index=False)
